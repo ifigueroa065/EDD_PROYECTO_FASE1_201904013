523d854422b14ab0368c9d58f10147ca4aa61dda
Roafnisma <isaiimiff13@gmail.com>
2022-03-08 14:15:12 -0600

    init_f2

--- FASE2/UDRAWING_PAPER/src/udrawing_paper/UDRAWING_PAPER.java
+++ FASE2/UDRAWING_PAPER/src/udrawing_paper/UDRAWING_PAPER.java
@@ -13,6 +13,7 @@ public class UDRAWING_PAPER {
     public static void main(String[] args) {
         // TODO code application logic here
            System.out.println("Hello");
+           System.out.println("AYOLA");
     }
     
 }
